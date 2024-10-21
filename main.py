a10c59ee73104d95d34d623a835e0f401976aa5c
Muhammadu Barry <74269375+MuhammaduBarry@users.noreply.github.com>
2024-10-21 06:23:49 -0400

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image, ImageFilter
-
-# JPG to PNG converter
-img = Image.open('./pokemon/pikachu.jpg')
-
-filtered_img = img.convert('L')
-filtered_img.save('gray', 'png')
-filtered_img.show()
